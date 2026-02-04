01918fb242b13759d9c493b51926b8ffb52cde7d
Akshitha12-25 <akshithanalla12@gmail.com>
2026-02-05 01:02:07 +0530

    Task 3: Implemented Todo CRUD APIs

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use("/api/todos", require("./routes/todoRoutes"));
+app.use("/api/todos", require("./routes/todoRoutes"));
 
 
 app.get("/", (req, res) => {
